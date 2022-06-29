91213258723960d61f200f75c4590dbad5180a15
Chanponleusopheaho <chanponleusopheaho@gmail.com>
2022-06-29 14:49:48 +0700

    Add Complex number

--- Week 2.py
+++ Week 2.py
@@ -1,5 +1,11 @@
-Number_1= int(input("Enter number"))
-Number_2= int(input("Enter number"))
-Number_3= int(input("Enter number"))
+Number_1= int(input("Enter number:"))
+Number_2= int(input("Enter number:"))
+Number_3= int(input("Enter number:"))
 Total = (Number_1+Number_2)/Number_3
 print("The total is:",Total)
+# Complex number
+# in math the complex number is with "i" but in programming they will use "J"
+X = 12
+Y = 32
+Z = complex(X,Y)
+print(Z)
