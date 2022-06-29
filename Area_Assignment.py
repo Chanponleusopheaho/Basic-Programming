b2fa4f41b7a9b320bdde659b26a9a363931db8fc
Chanponleusopheaho <chanponleusopheaho@gmail.com>
2022-06-29 17:40:02 +0700

    Completed

---
+++ Area_Assignment.py
@@ -0,0 +1,12 @@
+# The area of Rectangle 
+Length = 31 # value in meter
+Width = 16.5 # value in meter
+Area_of_Rectangle = float(Length*Width)
+print("The area of Retangle =",Area_of_Rectangle,"meter^2")
+
+#The area of Circle
+import math
+Diameter = 34 # value in meter
+Radius = Diameter/2
+Area_of_Circle = (math.pi)*Radius**2
+print("The area of the circle =",Area_of_Circle,"meter^2")
