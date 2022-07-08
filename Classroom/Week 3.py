33d76f6b506cbe4c528bf5cc181ad20efac09bc5
Chanponleusopheaho <chanponleusopheaho@gmail.com>
2022-07-08 10:39:12 +0700

    Updated

--- Classroom/Week 3.py
+++ Classroom/Week 3.py
@@ -2,3 +2,4 @@ Name = input (" What is your name? ")
 Age = input (" How old are you? ")
 print("So your name is:",Name)
 print("Also your age is",Age)
+# Nested IF statement is one condition in another condition 
