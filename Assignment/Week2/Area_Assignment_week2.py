5e85a4543c4bc454c410f70d034948736eff3f5c
Chanponleusopheaho <chanponleusopheaho@gmail.com>
2022-07-08 10:32:13 +0700

    Updated

--- Assignment/Week2/Area_Assignment_week2.py
+++ Assignment/Week2/Area_Assignment_week2.py
@@ -2,7 +2,7 @@
 Length = 31 # value in meter
 Width = 16.5 # value in meter
 Area_of_Rectangle = float(Length*Width)
-print("The area of Retangle =",Area_of_Rectangle,"meter^2")
+print("The area of Retangle =",Area_of_Rectangle,"m\u00b2")
 # The circumference of Rectangle
 Circumference_of_Rectangle = float(2*(Length+Width))
 print("The circumference of Retangle =",Circumference_of_Rectangle,"meter")
@@ -12,7 +12,7 @@ import math
 Diameter = 34 # value in meter
 Radius = Diameter/2
 Area_of_Circle = (math.pi)*Radius**2 
-print("The area of the circle =",Area_of_Circle,"meter^2")
+print("The area of the circle =",Area_of_Circle,"m\u00b2")
 #The circumference of Circle
 Circumference_of_Circle = float(2 * (math.pi) * Radius)
 print("The circumference of the circle =",Circumference_of_Circle,"meter")
