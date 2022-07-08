d800851331edbb6440ecbdc112327504fb663d54
Chanponleusopheaho <chanponleusopheaho@gmail.com>
2022-07-08 10:36:52 +0700

    New

--- Assignment/Week3/Prime_number.py
+++ Assignment/Week3/Prime_number.py
@@ -1,8 +1,8 @@
-number = 1
-while number <= 100:
+number = 0
+while number <= 99:
     number +=1
-    for a in range(1,number): 
-        if (number % a) == 0:
+    for a in range(2,number): 
+        if number % a == 0:
             print (number, " is not a prime number")
     else: 
         print ( number," is a prime number")
