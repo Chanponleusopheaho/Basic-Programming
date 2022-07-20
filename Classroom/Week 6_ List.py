c62f3b473444115d769c65bb2fffbdae9524d9b2
Chanponleusopheaho <chanponleusopheaho@gmail.com>
2022-07-20 14:31:24 +0700

    New

---
+++ Classroom/Week 6_ List.py
@@ -0,0 +1,9 @@
+L = [10, 20, 30, 20, 40, 20, 50]
+firstIndex = L.index(20)
+secondIndex = L.index(20, firstIndex + 1)
+print(firstIndex)
+print(secondIndex)
+
+List = ["Hello",[1,2,3,4]]
+print(List[0])
+print(List[0][1]) # The second [1] is the index in the first index " Nested "
