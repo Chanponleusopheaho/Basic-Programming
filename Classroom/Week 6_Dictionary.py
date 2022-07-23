434c29912d9df78cc499d8b67b31a34f9285668d
Chanponleusopheaho <chanponleusopheaho@gmail.com>
2022-07-23 22:33:48 +0700

    New

---
+++ Classroom/Week 6_Dictionary.py
@@ -0,0 +1,20 @@
+# Dictionary with mixed key
+dic2 = {'name':'john', 1: [2,3,4]}
+print(dic2)
+
+# form sequence having each item as a pair 
+dic3 = dict([(1,'hi'),(2,'ball')])
+print(dic3)
+
+#retriveing elements 
+dic4 = {'name':'Jack', 'age':26}
+print(dic4['name'])
+
+# Changing and adding Dictionary element
+dic4['age'] = 27
+print(dic4)
+
+# add item 
+dic4 ['Address'] = "Downtown"
+print(dic4)
+
