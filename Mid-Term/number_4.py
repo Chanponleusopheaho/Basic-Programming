8997a4a5a1b2e06af8d2aa31a4c7348922d035f8
Chanponleusopheaho <chanponleusopheaho@gmail.com>
2022-07-29 10:28:02 +0700

    new

--- Mid-Term/number_4.py
+++ Mid-Term/number_4.py
@@ -1,5 +1,7 @@
 num = int(input("Enter a number:"))
-if num == 36 and 72:
-    print("This number is divisible by 4 and 9.")
-else:
+if num % 4 == 0 :
+    print ("This number is divisible by 4 and 9.") 
+elif num % 9 == 0:
+    print ("This number is divisible by 4 and 9.")
+else: 
     print("This number is not divisible by 4 and 9.")
